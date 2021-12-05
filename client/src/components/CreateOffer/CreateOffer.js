49906e962bae1d3ee5dddf779fc9592f53f27b8b
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-12-05 18:57:17 +0200

    Add offers pages

---
+++ client/src/components/CreateOffer/CreateOffer.js
@@ -0,0 +1,17 @@
+import PrimaryNavigation from '../Navigations/PrimaryNavigation/PrimaryNavigation';
+
+const CreateOffer = () => {
+
+    return (
+        <>
+            <header>
+                <PrimaryNavigation />
+            </header>
+            <main>
+                <h1>Create Offer</h1>
+            </main>
+        </>
+    )
+}
+
+export default CreateOffer;
---
+++ client/src/components/Offers/Offers.js
@@ -0,0 +1,17 @@
+import PrimaryNavigation from '../Navigations/PrimaryNavigation/PrimaryNavigation';
+
+const Offers = () => {
+
+    return (
+        <>
+            <header>
+                <PrimaryNavigation />
+            </header>
+            <main>
+                <h1>Offers</h1>
+            </main>
+        </>
+    )
+}
+
+export default Offers;
