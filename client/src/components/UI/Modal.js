0af98ced57bde33e60219490730bac4273033c10
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-12-19 17:07:19 +0200

    Fix create form re-rendering

--- client/src/components/UI/Modal.js
+++ client/src/components/UI/Modal.js
@@ -1,28 +1,18 @@
-import ReactDOM from 'react-dom';
-
-const Backdrop = props => {
-    return (
-        <div className="backdrop" onClick={props.onClose}></div>
-    );
-}
-
-const ModalOverlay = props => {
-    return (
-        <div className="modal">
-            <div className="content">{props.children}</div>
-        </div>
-    );
-}
-
-const portalElement = document.getElementById('overlays');
+import './Modal.css';
 
 const Modal = props => {
+
+    const modalClickHandler = (e) => {
+        e.stopPropagation();
+    }
+
     return (
-        <>
-        {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
-        </>
-    )
+        <div className="backdrop" onClick={props.onCancel}>
+            <div className="modal" onClick={modalClickHandler}>
+                <div className="content">{props.children}</div>
+            </div>
+        </div>
+    );
 }
 
 export default Modal;
