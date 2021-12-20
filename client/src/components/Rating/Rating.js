7da35a81ccb090375b44825ec1756f8bb5aec555
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-12-20 19:14:47 +0200

    Add star rating

---
+++ client/src/components/Rating/Rating.js
@@ -0,0 +1,24 @@
+import './Rating.css';
+
+const Rating = ({ onRate }) => {
+    const getRatingHandler = (e) => {
+        onRate(e.target.htmlFor.split('-')[1]);
+    }
+
+    return (
+        <div className="rating">
+            <input type="radio" className="rating" id="rating-5" />
+            <label htmlFor="rating-5" onClick={getRatingHandler}></label>
+            <input type="radio" className="rating" id="rating-4" />
+            <label htmlFor="rating-4" onClick={getRatingHandler}></label>
+            <input type="radio" className="rating" id="rating-3" />
+            <label htmlFor="rating-3" onClick={getRatingHandler}></label>
+            <input type="radio" className="rating" id="rating-2" />
+            <label htmlFor="rating-2" onClick={getRatingHandler}></label>
+            <input type="radio" className="rating" id="rating-1" />
+            <label htmlFor="rating-1" onClick={getRatingHandler}></label>
+        </div>
+    );
+}
+
+export default Rating;
