3b247c72d208b9a3c2b74973923401d18d4797e8
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-12-22 15:51:13 +0200

    Add mobile navigation

--- client/src/components/Navigations/PrimaryNavigation/PrimaryNavigation.js
+++ client/src/components/Navigations/PrimaryNavigation/PrimaryNavigation.js
@@ -1,39 +1,50 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../../contexts/AuthContext';
+
+import openMenu from '../assets/icon-hamburger.svg';
+import closeMenu from '../assets/icon-close.svg';
 
 import './PrimaryNavigation.css';
 
 const PrimaryNavigation = () => {
     const { isAuthenticated } = useAuth();
 
-    return (
-        <div className="flex primary-navigation container">
-            <Link to="/"><span className="text-accent ff-accent primary-navigation-logo">worklance</span></Link>
+    const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+    const showMenuHandler = (e) => {
+        setIsMenuExpanded(!isMenuExpanded);
+    }
 
-            <div>
-                {/* <button onClick={showMenuHandler} className="mobile-nav-toggle" aria-controls="primary-navigation"><span className="sr-only" aria-expanded={isMenuExpanded}>Menu</span><img src={isMenuExpanded ? closeMenu : openMenu} alt="" /></button> */}
-                <nav>
-                    <ul className="flex header-nav" /*id="primary-navigation"  className={`primary-navigation underline-indicators flex ${isMenuExpanded ? 'expand-menu' : ''}`}*/>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/offers">Offers</Link></li>
-                        {isAuthenticated
-                            ?
-                            <>
-                                <li><Link to="/create-offer">Create Offer</Link></li>
-                                <li><Link to="/profile">Profile</Link></li>
-                                <li><Link to="/logout">Logout</Link></li>
-                            </>
-                            :
-                            <>
-                                <li><Link to="/login">Login</Link></li>
-                                <li><Link to="/register">Join</Link></li>
-                            </>
-                        }
-                    </ul>
-                </nav>
+    return (
+        <nav className="flex primary-navigation container">
+            <div className="flex logo-container">
+                <Link to="/"><span className="text-accent ff-accent primary-navigation-logo">worklance</span></Link>
+
+                <div onClick={showMenuHandler} className="menu-icon-container">
+                    {isMenuExpanded ? <img src={closeMenu} alt="Close menu" /> : <img src={openMenu} alt="Open menu" />}
+                </div>
             </div>
-        </div>
+
+
+            <ul className={`header-nav ${isMenuExpanded ? 'expanded' : ''}`}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/offers">Offers</Link></li>
+                {isAuthenticated
+                    ?
+                    <>
+                        <li><Link to="/create-offer">Create Offer</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link to="/logout">Logout</Link></li>
+                    </>
+                    :
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Join</Link></li>
+                    </>
+                }
+            </ul>
+        </nav>
     )
 }
 
