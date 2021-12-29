fe9acc5478ea9ee93da40839b8ef88f2556714d6
Polina Kerkenyakova <polina.kerkenyakova@gmail.com>
2021-12-29 10:59:22 +0200

    Expose constants for heroku build

---
+++ src/config/contants.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 5000;
+
+export const DB_CONNECTION_STRING = 'mongodb+srv://polinakerkenyakova:finettii@worklance.iqlxc.mongodb.net/worklance';
+
+export const TOKEN_SECRET = 'secret';
+
+export const COOKIE_NAME = 'SESSION_TOKEN';
